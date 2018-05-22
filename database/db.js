bd313b9cbbaf8da954831e767d8c29156b29a613
THIZY Kevin <kevin.thizy@outlook.fr>
2018-05-22 09:40:46 +0200

    Removed secret.js for database.

--- database/db.js
+++ database/db.js
@@ -1,15 +1,19 @@
 'use strict';
 const mongoose = require('mongoose');
-const config = { host: (process.env.MONGO_URL || require('./secret.js').host).trim() };
+const config = { host: (process.env.MONGO_URL || "mongodb://localhost:27017/arachne") };
 
 let connected = false;
 
 exports.connect = function() {
   if (!connected) {
     console.log("> [INFO] \x1b[36mConnecting to mongodb database...\x1b[0m");
-    mongoose.connect(config.host);
-    console.log("> [INFO] \x1b[42mConnected to mongodb database!\x1b[0m")
-    return connected = true;
+    return mongoose.connect(config.host).then(() => {
+      console.log("> [INFO] \x1b[42mConnected to mongodb database!\x1b[0m")
+      return connected = true;
+    }).catch((err) => {
+      console.log(err);
+      console.log("> [ERROR] Could not connect to mongodb database!")
+    });
   }
   console.log("> [WARNING] Already connected to mongod database.")
   return connected = true;
