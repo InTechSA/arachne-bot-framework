4f9c0df57a6ae5ac13abdcac87fe6f830877d6ca
THIZY Kevin <kevin.thizy@outlook.fr>
2018-06-25 11:37:07 +0200

    Updated Skills.

--- logic/skills/help/skill.js
+++ logic/skills/help/skill.js
@@ -17,21 +17,21 @@ module.exports = (skill) => {
             }).then((response) => {
                 var helpSkills = response.data.skills;
                 var text = "";
-                var helpCmd;
                 switch(phrase[0]) {
                     case "":
                         var j;
                         for(var helpSkill of helpSkills) {
                             if(helpSkill.active) {
-                                for(helpCmd of helpSkill.commands) {
+                                for(var helpCmd of helpSkill.commands) {
                                     text += "- `!" + helpCmd.cmd + "` -> " + helpCmd.help.description + "\n";
                                 }
                             }
                         }
-                        text += "Pour consultez l'help de chaque commande, tapez `!help [cmd]`, example : `!help git`";
+                        text += "Pour consultez l'help de chaque commande, tapez `!help [cmd]`, example : `!help git`\nExplorez l'aide en ligne: https://arachne-bot.intech-lab.com/manual";
                         return({
                             message: {
                                 title: "Help of R2D2",
+                                title_url: "https://arachne-bot.intech-lab.com/manual",
                                 text
                             }
                         });
@@ -52,7 +52,7 @@ module.exports = (skill) => {
                                 }
                             });
                         } else {
-                            helpCmd = helpCmds.filter((cmd) => cmd.cmd === command)[0];
+                            var helpCmd = helpCmds.filter((cmd) => cmd.cmd === command)[0];
                             var title = "Aide de " + helpCmd.cmd;
                             text += "Description : " + helpCmd.help.description + "\n";
                             if(helpCmd.help.subcommands) {
@@ -80,7 +80,7 @@ module.exports = (skill) => {
                                     text += "\n";
                                 }
                             } else {
-                                text += "Pas de sous commande ! Utilisez juste `!"+helpCmd.cmd+"`";
+                                text += "Pas de sous commande ! Utilisez juste `!"+helpCmd.cmd+"`"
                             }
                             return({
                                 message: {
@@ -94,19 +94,6 @@ module.exports = (skill) => {
                 skill.log(err.message);
                 throw "Une erreur est survenue durant l'appel des helps des skills du bot :("; 
             });
-        // Build the help
-        /* buildHelp = "Voici la liste des commandes disponibles : \n";
-        buildHelp += "- `!bus` *→* Donne les horaires des prochains bus passant à l'agence.\n";
-        buildHelp += "- `!events soon|all|[query]` *→* affiche les évenements d'ActIntech prochainement | tous | d'après une recherche.\n";
-        buildHelp += "- `!itinary [dest1] [dest2]` *→* affiche un itinéraire d'une destination 1 à une destination 2. Si la destination 2 est absente, fais un départ de l'agence.\n";
-        buildHelp += "- `!quizz` *→* Lance un petit quizz.\n";
-        buildHelp += "- `!vacation _|request|status` *→* si aucun paramètre n'est fournie, affiche les congés disponible pour la personne qui l'a demandé. Request lance une demande de congée automatisée. Status renvoie les statues des différentes demandes de congés faites par le biais du bot.\n";
-        buildHelp += "- `!whois [TRI]|[username]` *→* renvoie des informations sur une personne en donnant son trigramme ou son username ( prénom.nom ). Si l'utilisateur n'a pas été trouvé, renvoie une liste de suggestions d'utilisateurs.\n";
-        buildHelp += "- `!git attach|detach|list` *→* Gère des webhooks pour gitlab (alertes pour push/merge et pipelines)\n";
-        buildHelp += "- `!brokkr` *→* Accéder à dokku et gérer vos merge-request sur gitlab.\n";
-        buildHelp += "- `!alarms` *→* Créer des alarmes.\n";
-        buildHelp += "- `!tea green|black|herbs` *→* Soyez alerté pour votre thé ou infusion.\n";
-        buildHelp += "- `!croissant _|moi` *→* Qui est le prochain duo des croissants ?\n";*/
         }).catch((err) => {
             if(typeof(err) !== String) err = err.toString();
             skill.log("Error : " + err);
@@ -121,8 +108,8 @@ module.exports = (skill) => {
         description: "help command"
     });
     
-    skill.addIntent("help","get-help",({ entities: { command: command = "" } , phrase , data }) => {
+    skill.addIntent("help","get-help",({ entities: { command: command = [""] } , phrase , data }) => {
         skill.log("NLP");
-        skill.handleCommand("help",{phrase: "help "+command,data});
+        return skill.handleCommand("help",{phrase: command[0],data});
     });
 };
